37d2c56c107128c6ae257ee4ad6afa3a26898f58
CAI-JZ <roland.caijingzhu@gmail.com>
2022-04-30 20:29:41 +0100

    11

--- Assets/Script/cs_Obstruct2D.cs
+++ Assets/Script/cs_Obstruct2D.cs
@@ -10,7 +10,7 @@ public class cs_Obstruct2D : cs_Item
         int Pos = Random.Range(1,5);
         //print(Pos);
         float ScaleX1 = Random.Range(1f,1.2f);
-        float ScaleX2 = Random.Range(0.4f, 0.8f);
+        float ScaleX2 = Random.Range(0.6f, 1f);
         float Offset = Random.Range(-0.05f, 0.05f);
         switch (Pos)
         {
